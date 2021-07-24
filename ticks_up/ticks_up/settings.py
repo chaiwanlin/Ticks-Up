b9785e919cc1054f1976428c6fd5b51229351d8c
chaiwanlin <cwnlnh@gmail.com>
2021-07-25 03:48:24 +0800

    added whitenoise middleware

--- ticks_up/ticks_up/settings.py
+++ ticks_up/ticks_up/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ticks_up.urls'
@@ -135,10 +136,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, './static'),
+    os.path.join(BASE_DIR, './staticfiles'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
